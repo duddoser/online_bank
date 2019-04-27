f8a0df1a11e266faf2240487d9f300a47b6567cf
duddoser <45271863+duddoser@users.noreply.github.com>
2019-04-27 13:16:36 +0300

    Update usersmodel.py

--- usersmodel.py
+++ usersmodel.py
@@ -7,24 +7,35 @@ class UsersModel:
         cursor = self.connection.cursor()
         cursor.execute('''CREATE TABLE IF NOT EXISTS users 
                             (id INTEGER PRIMARY KEY AUTOINCREMENT, 
-                             user_name VARCHAR(50),
-                             password_hash VARCHAR(128)
+                             login VARCHAR(50),
+                             password_hash VARCHAR(128),
+                             card_number VARCHAR(16),
+                             expiry_date VARCHAR(8),
+                             name VARCHAR(50),
+                             safe_number(3),
+                             money(30) 
                              )''')
         cursor.close()
         self.connection.commit()
 
-    def insert(self, login, password_hash, credit_card_number, expiry_date, name, safe_number):
+    def insert(self, login, password_hash, card_number, expiry_date, name, safe_number):
         cursor = self.connection.cursor()
         cursor.execute('''INSERT INTO users 
-                          (login, password_hash, credit_card_number, expiry_date, name, safe_number) 
-                          VALUES (?,?,?,?,?,?)''', (login, password_hash, credit_card_number,
-                                                    expiry_date, name, safe_number))
+                          (login, password_hash, card_number, expiry_date, name, safe_number, money) 
+                          VALUES (?,?,?,?,?,?,?)''', (login, password_hash, card_number,
+                                                      expiry_date, name, safe_number))
         cursor.close()
         self.connection.commit()
 
     def get(self, user_id):
         cursor = self.connection.cursor()
         cursor.execute("SELECT * FROM users WHERE id = ?", (str(user_id),))
+        row = cursor.fetchone()
+        return row
+
+    def get_money(self, user_id):
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT money FROM users WHERE id = ?", (str(user_id),))
         row = cursor.fetchone()
         return row
 
@@ -36,7 +47,7 @@ class UsersModel:
 
     def get_card_number(self, user_id):
         cursor = self.connection.cursor()
-        cursor.execute("SELECT credit_card_number FROM users WHERE id = ?", (str(user_id)))
+        cursor.execute("SELECT card_number FROM users WHERE id = ?", (str(user_id)))
         row = cursor.fetchone()
         return row
 
@@ -46,5 +57,3 @@ class UsersModel:
                        (user_name, password_hash))
         row = cursor.fetchone()
         return (True, row[0]) if row else (False,)
-
-
