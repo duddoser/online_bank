3efebf4a9fc72397e56714cd1b9c516899311ad6
duddoser <45271863+duddoser@users.noreply.github.com>
2019-04-29 00:30:18 +0300

    added new insert and get methods

--- usersmodel.py
+++ usersmodel.py
@@ -6,24 +6,24 @@ class UsersModel:
     def init_table(self):
         cursor = self.connection.cursor()
         cursor.execute('''CREATE TABLE IF NOT EXISTS users 
-                                (id INTEGER PRIMARY KEY AUTOINCREMENT, 
-                                 user_name VARCHAR(50),
-                                 password_hash VARCHAR(128),
-                                 card_number VARCHAR(16),
-                                 expiry_date VARCHAR(8),
-                                 name VARCHAR(50),
-                                 safe_number(3),
-                                 money(30)
-                                 )''')
+                                    (id INTEGER PRIMARY KEY AUTOINCREMENT, 
+                                     user_name VARCHAR(50),
+                                     password_hash VARCHAR(128),
+                                     card_number VARCHAR(16),
+                                     expiry_date VARCHAR(11),
+                                     name VARCHAR(50),
+                                     safe_number VARCHAR(3),
+                                     money INTEGER
+                                     )''')
         cursor.close()
         self.connection.commit()
 
-    def insert(self, login, password_hash, card_number, expiry_date, name, safe_number):
+    def insert(self, login, password_hash, card_number, expiry_date, name, safe_number, money):
         cursor = self.connection.cursor()
         cursor.execute('''INSERT INTO users 
-                          (login, password_hash, card_number, expiry_date, name, safe_number, money) 
-                          VALUES (?,?,?,?,?,?,?)''', (login, password_hash, card_number,
-                                                      expiry_date, name, safe_number))
+                          (user_name, password_hash, card_number, expiry_date, name, safe_number, money) 
+                          VALUES (?,?,?,?,?,?,?)''', (login, password_hash, card_number, expiry_date, name,
+                                                      safe_number, money))
         cursor.close()
         self.connection.commit()
 
@@ -38,6 +38,33 @@ class UsersModel:
         cursor.execute("SELECT money FROM users WHERE id = ?", (str(user_id),))
         row = cursor.fetchone()
         return row
+
+    def get_id(self, card_number):
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT id FROM users WHERE card_number = ?", (str(card_number),))
+        row = cursor.fetchone()
+        return row
+
+    def get_card_number(self, user_id):
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT card_number FROM users WHERE id = ?", (str(user_id),))
+        row = cursor.fetchone()
+        return row
+
+    def exist_card_number(self, card):
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT * FROM users WHERE card_number = ?",
+                       (str(card),))
+        row = cursor.fetchone()
+        return (True,) if row else (False,)
+
+    def exists(self, user_name, password_hash):
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT * FROM users WHERE user_name = ? AND password_hash = ?",
+                       (user_name, password_hash))
+        row = cursor.fetchone()
+        return (True, row[0]) if row else (False,)
+
 
     def get_all(self):
         cursor = self.connection.cursor()
