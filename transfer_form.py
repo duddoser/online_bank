fbde8651af8290c561c7562dca1a532fee584b9b
duddoser <45271863+duddoser@users.noreply.github.com>
2019-04-30 02:10:12 +0300

    Delete transfer_form.py

--- transfer_form.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
-
-
-class TransferForm(FlaskForm):
-        card_number = StringField('Номер карты (лица, которому переводим)', validators=[DataRequired()])
-        money = StringField('Сумма перевода', validators=[DataRequired()])
-        submit = SubmitField('Перевести')
